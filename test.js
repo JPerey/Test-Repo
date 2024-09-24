7e2abe98f4a8cec6a852b67789d11ae01e547477
jperey <jamjam.perey@gmail.com>
2024-09-24 06:13:16 -0700

    added a line of code for windows practices

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log("hi hi from windows")
+
+let testVar = 99;
+let testVarB = 1;
+
+function AddEm(a,b) {
+    let sum = testVar + testVarB;
+    console.log(sum)
+}
+
+AddEm(testVar, testVarB)
